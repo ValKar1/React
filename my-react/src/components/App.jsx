1d48609f9140e63d1f1457f7ce202641f538e4fa
Valerius Karch <valerius.karch@adversign-media.de>
2022-05-25 14:32:17 +0200

    UPDATED: render(), props, state & componentDidMount, livecycle, children

---
+++ my-react/src/components/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export class App extends React.Component {
+  state = {
+    counterValue: 3
+  };
+
+  componentDidMount() {
+    console.log('componentDidMount:')
+    this.myInterval = setInterval(() => {
+      // Modify state only with setState
+      this.setState((state) => {
+        if (state.counterValue > 0) {
+          return { counterValue: state.counterValue - 1 };
+        } else {
+          return { counterValue: 0 };
+        }
+      })
+    }, 1000)
+
+    // setInterval(() => {
+    //   // Modify state only with setState
+    //   this.setState({
+    //     counterValue: Math.max(0, this.state.counterValue -1),
+    //   })
+    // }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.myInterval)
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(prevProps, prevState, snapshot)
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    if (this.state.counterValue === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    const { title, name, nameObj } = this.props;
+    return <div title={title}>Hallo {nameObj.prename} {nameObj.lastname} <strong>Countdown = {this.state.counterValue}</strong>
+            <div>{this.props.children}</div>
+            <div>{this.props.coolKid}</div>
+           </div>
+  }
+}
--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {App} from './components/App';
+
+// import ReactD from 'react-dom';
+// Babel live editor: https://babeljs.io/repl
+// ReactD.render(
+//   React.createElement('p', null, 'Hallo Welt!!!'),
+//   document.getElementById('root')
+// )
+
+const nameObj = {
+  prename: 'Vorname',
+  lastname: 'Nachname'
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// JSX
 root.render(
   <React.StrictMode>
-    <div>Hello World</div>
+    <App title="test" name="linkedIn" nameObj={nameObj} coolKid={<p>Cool!</p>}>
+      <p>Die Learning Cat ist eine wissbegierige Katze</p>
+    </App>
   </React.StrictMode>
 );
